c9172b63a86249a1d07d8184078199647dde0d62
Kevin Poorman <kpoorman@salesforce.com>
2021-07-14 10:15:10 -0400

    Removing commented out test

--- force-app/main/default/lwc/makeASuggestion/__tests__/makeASuggestion.test.js
+++ force-app/main/default/lwc/makeASuggestion/__tests__/makeASuggestion.test.js
@@ -13,20 +13,6 @@ jest.mock(
 );
 
 describe('c-make-a-suggestion', () => {
-    // it('should render link to contribution.md when github username === trailheadapps', () => {
-    //     const element = createElement('c-make-a-suggestion', {
-    //         is: MakeASuggestion
-    //     });
-
-    //     element.groupName = 'Files Recipes';
-    //     element.recipeName = 'FilesRecipes';
-    //     document.body.appendChild(element);
-    //     return Promise.resolve().then(() => {
-    //         const suggestionEl = element.shadowRoot.querySelector('a');
-    //         expect(suggestionEl.href).toContain('CONTRIBUTION.md');
-    //     });
-    // });
-
     it('should render a link to the users fork and the proper file when github username !== trailheadapps', () => {
         // eslint-disable-line max-len
         const element = createElement('c-make-a-suggestion', {
